790ea83d5ca4b58e145608ceb5b1ee15c2c289e5
Kirill Ushkalov <kirill.ushkalov@banuba.com>
2023-11-15 13:02:04 +0300

    format

--- src/settings/background.js
+++ src/settings/background.js
@@ -9,24 +9,24 @@ export default {
     reset() {
       this.background.reset()
     },
-    async setBackground(file){
-      const tag = await exifr.orientation(file);
+    async setBackground(file) {
+      const tag = await exifr.orientation(file)
       console.log(tag)
-      let rotation = 0;
-      switch(tag){
-        case 3: 
-          rotation = 180;
-          break;
+      let rotation = 0
+      switch (tag) {
+        case 3:
+          rotation = 180
+          break
         case 6:
-          rotation = 90;
-          break;
+          rotation = 90
+          break
         case 8:
-            rotation = 260;
-          break;
+          rotation = 260
+          break
         default:
-          break;
+          break
       }
-      background.update({texture: file, rotation: rotation})
+      background.update({ texture: file, rotation: rotation })
     }
   },
   components: { BnbSetting, BnbSlider },
